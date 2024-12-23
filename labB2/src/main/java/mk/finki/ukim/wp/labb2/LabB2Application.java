c1a0b74225d8162893e86637108920004f15a961
Matej Ivankovikj <ivankovikj.matej@gmail.com>
2024-12-23 19:46:17 +0100

    Lab4

--- labB2/src/main/java/mk/finki/ukim/wp/labb2/LabB2Application.java
+++ labB2/src/main/java/mk/finki/ukim/wp/labb2/LabB2Application.java
@@ -2,12 +2,23 @@ package mk.finki.ukim.wp.labb2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 @SpringBootApplication
+@ServletComponentScan
 public class LabB2Application {
 
     public static void main(String[] args) {
         SpringApplication.run(LabB2Application.class, args);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
     }
 
 }
